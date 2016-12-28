f9bfbf94d2272455a86090be37a91553d8eb77fe
dwissk <dominik.wisskirchen@ebf.de>
2016-12-28 22:53:36 +0100

    change interval

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-cron.schedule('*/60 * * * *', function(){
+cron.schedule('*/59 * * * *', function(){
   radiationService.check();
 });
 radiationService.check();
